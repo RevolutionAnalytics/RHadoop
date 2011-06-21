c93b6142c6ef852d7c14e283c3340c2209d3e17d
Antonio Piccolboni <antonio@rightload.info>
2011-06-21 11:47:22 -0700

    simple examples of rhstream usage

---
+++ rhstream/pkg/inst/basic-examples.R
@@ -0,0 +1,22 @@
+rawtextinputformat = function(line) {keyval(NULL, line)}
+             
+mrwordcount = function (infile, outfile, pattern = " ") {
+  rhstream(in.folder = infile ,
+           out.folder = outfile,
+           textinputformat = rawtextinputformat,
+           map = function(k,v) {
+             lapply(
+                    strsplit(
+                             x = v,
+                             split = pattern)[[1]],
+                    function(w) keyval(w,1))},
+           reduce = function(k,vv) {
+             keyval(k, sum(unlist(vv)))})}
+
+filtermap= function(pred) function(k,v) {if (pred(v)) keyval(k,v) else NULL}
+
+mrfilter = function (infile, outfile, pred) {
+  rhstream(in.folder = infile ,
+           out.folder = outfile,
+           map = filtermap(pred))
+         }
