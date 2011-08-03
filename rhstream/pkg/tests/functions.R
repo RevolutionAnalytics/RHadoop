25d911811a1ffaacb8fc8e62ec5f5293bbbfba6d
piccolbo <antonio@piccolboni.info>
2011-08-03 16:20:43 -0700

    written test for test data generators

--- rhstream/pkg/tests/functions.R
+++ rhstream/pkg/tests/functions.R
@@ -5,18 +5,30 @@ library(RevoHStream)
 test = function(property, iterations=10, condition = function(...) T, generators = list()) {
   lapply(1:100, function(i) {
     args = lapply(generators, function(a) do.call(a, list()))
-    if(condition(args) && !do.call(property, args)) stop(deparse(list(property, args)))})
+    if(do.call(condition, args) && !do.call(property, args)) stop(deparse(list(property, args)))})
   paste ("Pass ", paste(deparse(property), collapse = " "))}
 
 ## random data  generators
 
-constant = function(const) function() const
-rselect = function(l) function() sample(l,1)
-rnumber = function(distribution, ...) function() distr(1,...)
+rdgconstant = function(const) function() const
+rdgselect = function(l) function() sample(l,1)
+rdgunion = function(...) function() sample(list(...),1)[[1]]()
+rdgdistribution = function(distribution, ...) function() distribution(1, ...)
 
-rnumericlist = function(lambda) function() lapply(1:rpois(1,lambda), runif(1))
+rdgnumericlist = function(lambda) function() lapply(1:rpois(1,lambda), function(i) runif(1))
 
-rnumerickeyvallist = function(lambda) function() lapply(1:rpois(1,lambda), function(i) keyval(runif(2)))
+rdgnumerickeyvallist = function(lambda) function() lapply(1:rpois(1,lambda), function(i) keyval(runif(2)))
+
+## test of the tests
+
+test(function(x) rdgconstant(x)() == x, generators = list(rdgdistribution(runif)))
+test(function(l) is.element(rdgselect(l)(), l), generators = list(rdgnumericlist(10)))
+test(function(n) is.element(rdgunion(rdgconstant(n), rdgconstant(2*n))(), list(n,2*n)), 
+     generators = list(rdgdistribution(runif)))
+test(function() {
+  rdgd = rdgdistribution(runif)
+  ks.test(runif(10000), sapply(1:10000, function(i) rdgd()))$p > 0.001})
+
 
 
 ## actual tests
@@ -24,4 +36,4 @@ test(function(kvl) all.equal(kvl,
                              apply(cbind(getKeys(kvl), 
                                          getValues(kvl)),1,keyval)), 
      iterations = 10, 
-     generators = list(rnumerickeyvallist(10)))
+     generators = list(rdgnumerickeyvallist(10)))
