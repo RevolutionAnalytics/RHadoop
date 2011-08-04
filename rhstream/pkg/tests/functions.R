1068a8c83e1225fce290c3f79755e332c3fea015
piccolbo <antonio@piccolboni.info>
2011-08-04 11:44:47 -0700

    more tests

--- rhstream/pkg/tests/functions.R
+++ rhstream/pkg/tests/functions.R
@@ -2,7 +2,8 @@ library(RevoHStream)
 
 ##main function, should be factored out one day
 
-test = function(property, iterations=10, condition = function(...) T, generators = list()) {
+unittest = function(property, iterations=10, condition = function(...) T, generators = list()) {
+  set.seed(0)
   lapply(1:100, function(i) {
     args = lapply(generators, function(a) a())
     if(do.call(condition, args) && !do.call(property, args)) stop(deparse(list(property, args)))})
@@ -17,24 +18,40 @@ rdgdistribution = function(distribution, ...) function() distribution(1, ...)
 
 rdgnumericlist = function(lambda) function() lapply(1:rpois(1,lambda), function(i) runif(1))
 
+rdgkeyval = function() function() keyval(runif(1), runif(1))
 rdgnumerickeyvallist = function(lambda) function() lapply(1:rpois(1,lambda), function(i) keyval(runif(2)))
 
 ## test of the generators
 
-test(function(x) rdgconstant(x)() == x, generators = list(rdgdistribution(runif)))
-test(function(l) is.element(rdgselect(l)(), l), generators = list(rdgnumericlist(10)))
-test(function(n) is.element(rdgmixture(rdgconstant(n), rdgconstant(2*n))(), list(n,2*n)), 
+unittest(function(x) rdgconstant(x)() == x, generators = list(rdgdistribution(runif)))
+unittest(function(l) is.element(rdgselect(l)(), l), generators = list(rdgnumericlist(10)))
+unittest(function(n) is.element(rdgmixture(rdgconstant(n), rdgconstant(2*n))(), list(n,2*n)), 
      generators = list(rdgdistribution(runif)))
-test(function(d) {
+unittest(function(d) {
   rdgd = rdgdistribution(d)
   ks.test(d(10000), sapply(1:10000, function(i) rdgd()))$p > 0.001},
      generators = list(rdgselect(list(runif, rnorm))))
 
-
-
+## for short
+catch.out = function(...) capture.output(invisible(...))
 ## actual tests
-test(function(kvl) all.equal(kvl, 
+library(RevoHStream)
+##createReader
+##pending input redirect issue
+## send
+unittest(function(kv) RevoHStream:::defaulttextoutputformat(kv$key, kv$value) == 
+                      catch.out(RevoHStream:::send(kv))),
+         iterations = 10,
+         generators = list(rdgkeyval()))
+unittest(function(lkv) {
+  catch.out(lapply(lkv,RevoHStream:::send)) ==
+  catch.out(RevoHStream:::send(lkv)))
+})
+##counter and status -- unused for now
+##getKeys and getValues
+unittest(function(kvl) all.equal(kvl, 
                              apply(cbind(getKeys(kvl), 
                                          getValues(kvl)),1,keyval)), 
      iterations = 10, 
      generators = list(rdgnumerickeyvallist(10)))
+##keyval
