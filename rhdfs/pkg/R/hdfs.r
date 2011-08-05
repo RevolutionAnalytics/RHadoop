1e50b44c85afcdb66bd942d27e76dff8b8185c3a
piccolbo <antonio@piccolboni.info>
2011-08-05 13:49:36 -0700

    added copyright notices to all R files

--- rhdfs/pkg/R/hdfs.r
+++ rhdfs/pkg/R/hdfs.r
@@ -1,3 +1,18 @@
+# Copyright 2011 Revolution Analytics
+#    
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ 
 hdfs.init <- function(hadoop=NULL,conf=NULL,libs=NULL,contrib=NULL,verbose=FALSE){
   if(is.null(hadoop)) hadoop <- Sys.getenv("HADOOP_HOME")
   if(is.null(conf)) conf <- Sys.getenv("HADOOP_CONF") #sprintf(Sys.getenv,hadoop)
