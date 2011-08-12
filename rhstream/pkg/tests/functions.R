6bbff09dfb163727976b1e69b7b777050da2f7b5
piccolbo <antonio@piccolboni.info>
2011-08-12 12:45:59 -0700

    lots of clean up work in meta generators, recursive metagenerator for lists
    Still failing the last test, but I wanted to sync since the work is good
    and the bug is in RJSONIO

--- rhstream/pkg/tests/functions.R
+++ rhstream/pkg/tests/functions.R
@@ -25,61 +25,75 @@ unittest = function(predicate, generators, iterations=10, condition = function(.
                  paste(args, collapse = " ")))}})
   print(paste ("Pass ", paste(deparse(predicate), "\n", collapse = " ")))}
 
-## test data  generators, some generic and some specific to the task at hand
+## test data  generators generators, some generic and some specific to the task at hand
 ## basic types
-tdglogical = function(ptrue =.5) function() rbinom(1,1,ptrue) == 1
-tdginteger = function(lambda = 100) tdgdistribution(rpois, lambda) #why poisson? Why not? Why 100?
-tdgdouble = function(min = -1, max = 1)  tdgdistribution(runif, min, max)
-##tdgcomplex NAY
+tdgglogical = function(ptrue =.5) function() rbinom(1,1,ptrue) == 1
+tdgginteger = function(lambda = 100) {tdg = tdggdistribution(rpois, lambda); function() as.integer(tdg())} #why poisson? Why not? Why 100?
+tdggdouble = function(min = -1, max = 1)  tdggdistribution(runif, min, max)
+##tdggcomplex NAY
 library(digest)
-tdgcharacter = function(len = 8) function() substr(
+tdggcharacter = function(len = 8) function() substr(
   paste(
     sapply(runif(ceiling(len/32)), digest), 
     collapse=""), 
   1, len)
-tdgraw = function(len = 8) function() charToRaw(tdgcharacter(len)())
-tdgfixedlist = function(...) function() lapply(list(...), function(tdg) tdg())
-tdgmonomorphiclist = function(tdg, len) function() replicate(len(),tdg()) 
+tdggraw = function(len = 8) {tdg = tdggcharacter(len); function() charToRaw(tdg())}
+tdgglist = function(tdg = tdggany(listtdg = tdg, lambdalist = lambda, maxlevel = maxlevel), 
+                    lambda = 10, maxlevel = 20) function() {if(sys.nframe() < maxlevel) replicate(rpois(1, lambda),tdg(), simplify=FALSE) else list()}
+tdggvector = function(tdg, lambda) {ltdg = tdgglist(tdg, lambda); function() unlist(ltdg())}
 
 ## special distributions
-tdgconstant = function(const) function() const
-tdgselect = function(l) function() sample(l,1)[[1]]
-tdgdistribution = function(distribution, ...) function() distribution(1, ...)
+tdggfixedlist = function(...) function() lapply(list(...), function(tdg) tdg())
+tdggconstant = function(const) function() const
+tdggselect = function(l) function() sample(l,1)[[1]]
+tdggdistribution = function(distribution, ...) function() distribution(1, ...)
 
 ##combiners
-tdgmixture = function(...) function() sample(list(...),1)[[1]]()
+tdggmixture = function(...) function() sample(list(...),1)[[1]]()
+
+## combine everything
+tdggany = function(ptrue = .5, lambdaint = 100, min = -1, max = 1, lenchar = 8, lenraw = 8, lambdalist = 10, 
+                   listtdg = tdggany(), lambdavector = 10, maxlevel = 20, vectortdg = tdggdouble()) 
+  tdggmixture(tdgglogical(ptrue), 
+              tdgginteger(lambdaint), 
+              tdggdouble(min, max), 
+              tdggcharacter(lenchar), 
+  #           tdggraw(lenraw),
+              tdggvector(vectortdg, lambdavector),
+              tdgglist(listtdg, lambdalist, maxlevel))
 
 ##app-specific generators
-tdgnumericlist = function(lambda) function() lapply(1:rpois(1,lambda), function(i) runif(1))
-tdgkeyval = function() function() keyval(runif(1), runif(1)) #we can do better than this
-tdgkeyvallist = function(lambda) function() lapply(1:rpois(1,lambda), function(i) keyval(runif(2))) #this one needs work too
-tdgnestedkeyvallist = function(lambda) function() lapply(1:rpois(1,lambda), function(i) keyval(runif(1), list(list(runif(2), "a", list("b", 1)))))
+tdggnumericlist = function(lambda = 100) function() lapply(1:rpois(1,lambda), function(i) runif(1))
+tdggkeyval = function(keytdg = tdggdouble(), valtdg = tdggany()) function() keyval(keytdg(), valtdg())
+tdggkeyvalsimple = function() function() keyval(runif(1), runif(1)) #we can do better than this
+tdggkeyvallist = function(keytdg = tdggdouble(), valtdg = tdgglist(), lambda = 100) tdgglist(tdg = tdggkeyval(keytdg, valtdg), lambda=lambda)
+
 
 ## generator test thyself
-##tdglogical
+##tdgglogical 
 unittest(function(ptrue) {
   binom.test(
-    sum(replicate(1000,expr = tdglogical(ptrue)())),1000, ptrue,"two.sided")$p.value > 0.001},
-         generators = list(tdgdistribution(runif, min = .1, max = .9)))
-##tdginteger same as tdgdistribution
-##tdgdouble same as tdgdistribution
-##tdgcomplex NAY
-##tdgcharacter: test legnth, but is it uniform?
-unittest(function(l) nchar(tdgcharacter(l)())==l,
-         generators = list(tdginteger()))
+    sum(replicate(1000,expr = tdgglogical(ptrue)())),1000, ptrue,"two.sided")$p.value > 0.001},
+         generators = list(tdggdistribution(runif, min = .1, max = .9)))
+##tdgginteger same as tdggdistribution
+##tdggdouble same as tdggdistribution
+##tdggcomplex NAY
+##tdggcharacter: test legnth, but is it uniform?
+unittest(function(l) nchar(tdggcharacter(l)())==l,
+         generators = list(tdgginteger()))
     
 #tdgconstant
-unittest(function(x) tdgconstant(x)() == x, generators = list(tdgdistribution(runif)))
+unittest(function(x) tdggconstant(x)() == x, generators = list(tdggdistribution(runif)))
 #tdgselect
-unittest(function(l) is.element(tdgselect(l)(), l), generators = list(tdgnumericlist(10)))
+unittest(function(l) is.element(tdggselect(l)(), l), generators = list(tdggnumericlist(10)))
 #tdgmixture
-unittest(function(n) is.element(tdgmixture(tdgconstant(n), tdgconstant(2*n))(), list(n,2*n)), 
-     generators = list(tdgdistribution(runif)))
+unittest(function(n) is.element(tdggmixture(tdggconstant(n), tdggconstant(2*n))(), list(n,2*n)), 
+     generators = list(tdggdistribution(runif)))
 #tdgdistribution
 unittest(function(d) {
-  tdgd = tdgdistribution(d)
+  tdgd = tdggdistribution(d)
   ks.test(d(10000), sapply(1:10000, function(i) tdgd()))$p > 0.001},
-     generators = list(tdgselect(list(runif, rnorm))))
+     generators = list(tdggselect(list(runif, rnorm))))
 
 ## for short
 catch.out = function(...) capture.output(invisible(...))
@@ -88,27 +102,27 @@ catch.out = function(...) capture.output(invisible(...))
 
 library(RevoHStream)
 ##createReader
-##pending input redirect issue
+##pending input redirect issue: use textConnection
 ## send
 unittest(function(kv) RevoHStream:::defaulttextoutputformat(kv$key, kv$val) == 
                       paste(catch.out(RevoHStream:::send(kv)),"\n", sep = ""),
-                  generators = list(tdgkeyval()))
+                  generators = list(tdggkeyval()))
 unittest(function(lkv) {
   all(catch.out(lapply(lkv,RevoHStream:::send)) ==
   catch.out(RevoHStream:::send(lkv)))
 },
-        generators = list(tdgkeyvallist(10)))
+        generators = list(tdggkeyvallist(10)))
 ##counter and status -- unused for now
 ##getKeys and getValues
 unittest(function(kvl) isTRUE(all.equal(kvl, 
                              apply(cbind(getKeys(kvl), 
                                          getValues(kvl)),1,keyval))), 
-         generators = list(tdgkeyvallist(10)))
+         generators = list(tdggkeyvallist(10)))
 ##keyval
 unittest(function(kv) {
   isTRUE(all.equal(kv,keyval(kv$key,kv$val))) &&      
   attr(kv, "keyval")},
-  generators = list(tdgkeyval()))
+  generators = list(tdggkeyval()))
 
 ##rhread rhwrite
 rhreadwritetest = function(generator) {
@@ -117,8 +131,7 @@ rhreadwritetest = function(generator) {
     generators = list(generator),
     iterations = 10)}
 
-rhreadwritetest(tdgkeyvallist(10))
-rhreadwritetest(tdgnestedkeyvallist(10))
+rhreadwritetest(tdggkeyvallist(10))
 
 ##revoMapReduce
 
@@ -127,7 +140,7 @@ unittest(function(kvl) {
   kvl1 = rhread(revoMapReduce(input=rhwrite(kvl)))
   kvl1 = kvl1[order(unlist(getKeys(kvl1)))]
   isTRUE(all.equal(kvl, kvl1, tolerance=1e-4))},
-  generators = list(tdgkeyvallist(10)),
+  generators = list(tdggkeyvallist()),
   iterations = 10)
 
                                          
