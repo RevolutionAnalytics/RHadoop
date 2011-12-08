60eb6e5c44631c276ce978c0e1ac2d949df1e330
piccolbo <antonio@piccolboni.info>
2011-12-08 14:23:41 -0800

    basic scripts to start an rmr cluster on ec2 with whirr

---
+++ rmr/pkg/tools/whirr/rmr-master.sh
@@ -0,0 +1,17 @@
+branch=master
+sudo apt-get install -y r-base-core
+sudo R --no-save << EOF
+install.packages(c('methods', 'RJSONIO', 'itertools', 'digest'), repos =  "http://lib.stat.cmu.edu/R/CRAN")
+EOF
+
+curl  -L   https://github.com/RevolutionAnalytics/RHadoop/tarball/$branch | tar zx
+mv RevolutionAnalytics-RHadoop* RHadoop
+sudo R CMD INSTALL RHadoop/rmr/pkg/
+
+sudo su << EOF1 
+cat >> /etc/profile <<EOF
+ 
+export HADOOP_HOME=/usr/lib/hadoop
+
+EOF
+EOF1
