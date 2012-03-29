7eff623b5cabca536a2085046e6516426945664a
Antonio Piccolboni <antonio@piccolboni.info>
2012-03-29 14:24:58 -0700

    killing more bogus out from ls with patterns

--- rmr/pkg/R/mapreduce.R
+++ rmr/pkg/R/mapreduce.R
@@ -408,8 +408,7 @@ for (hdfscmd in c("mv", "cp", "rm", "rmr", "expunge", "put", "copyFromLocal", "m
 
 pretty.hdfs.ls = function(...) {
   ls.out = hdfs.ls(...)
-  if(ls.out[1,1] == "Found") 
-    ls.out = ls.out[-1,]
+  ls.out = ls.out[-grep("Found", ls.out[,1]),]
   if(class(ls.out) == "character") ls.out = t(ls.out)
   df = as.data.frame(ls.out)
   names(df) = c("mode", "links", "owner", "group", "size", "last.modified.date", "last.modified.time", "path")
