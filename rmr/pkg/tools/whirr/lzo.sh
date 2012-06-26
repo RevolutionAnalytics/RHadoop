2f7571cf0612f136e617e69b2dabdedd3f6fb5e9
Antonio Piccolboni <antonio@piccolboni.info>
2012-06-26 15:47:14 -0700

    updating cluster cfg, java broken as ever

--- rmr/pkg/tools/whirr/lzo.sh
+++ rmr/pkg/tools/whirr/lzo.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/usr/lib/jvm/java-6-openjdk/
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
 wget https://github.com/toddlipcon/hadoop-lzo/tarball/master
 tar xvzf master
 cd toddlipcon-hadoop-lzo-c7d54ff/
@@ -10,5 +10,6 @@ sudo apt-get install -y liblzo2-dev
 sudo apt-get install -y make
 ant compile-native tar
 sudo cp build/hadoop-lzo-0.4.15/hadoop-lzo-0.4.15.jar /usr/lib/hadoop/lib/
+sudo mkdir -p /usr/lib/hadoop/lib/native/
 tar -cBf - -C build/hadoop-lzo-0.4.15/lib/native/ . | sudo tar -xBvf - -C /usr/lib/hadoop/lib/native/
 sudo /etc/init.d//hadoop-0.20-tasktracker restart
