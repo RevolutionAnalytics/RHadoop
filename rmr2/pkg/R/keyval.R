181daa50169f81318f752c8a07864cb77f6fa87d
Antonio Piccolboni <antonio@piccolboni.info>
2012-09-25 10:47:12 -0700

    added dependency the right way

--- rmr2/pkg/R/keyval.R
+++ rmr2/pkg/R/keyval.R
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
-library(functional)
 
 has.rows = function(x) !is.null(nrow(x))
 all.have.rows = Curry(all.predicate, P = has.rows)
