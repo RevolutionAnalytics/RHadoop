1aa7119c900d75f650a998afd7b4890679bafdee
Antonio Piccolboni <antonio@piccolboni.info>
2012-12-19 14:13:11 -0800

    many improvements and knitr labels

--- rmr2/pkg/examples/ngram.R
+++ rmr2/pkg/examples/ngram.R
@@ -18,9 +18,7 @@
 # $WHIRR_HOME/bin/whirr  run-script --script ~/Projects/Revolution/RHadoop/rmr/pkg/tools/whirr/lzo.sh  --config ~/Projects/Revolution/RHadoop/rmr/pkg/tools/whirr/hadoop-ec2-lzo.properties
 
 library(rmr2)
-library(robustbase)
-#using 2.0.2
-#fake data
+## @knitr fake-data
 fake.size = 2000000
 writeLines(
   apply(
@@ -34,17 +32,18 @@ writeLines(
     function(x)paste(x, collapse = "\t")), 
   file("/tmp/fake-ngram-data", "w"))
   
-# fake data
 source = "/tmp/fake-ngram-data"
 # rmr.options(backend = "local")
 
-# real data
+## @knitr distcp
 # do  distcp and then scatter
-# hadoop distcp s3n://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@datasets.elasticmapreduce/ngrams/books/20090715/eng-all/1gram/ hdfs:///user/antonio/
+# hadoop distcp -m 100 s3n://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@datasets.elasticmapreduce/ngrams/books/20090715/eng-all/1gram/ hdfs:///user/antonio/
 
 #source = "hdfs:///user/antonio/1gram/data"
 rmr.options(backend = "hadoop")
+rmr.option(keyval.length = 10^5)
 
+## @knitr ngram.format
 ngram.format = function(lines){
   data = 
     as.data.frame(
@@ -54,37 +53,41 @@ ngram.format = function(lines){
   data$count = as.integer(as.character(data$count))
   data}
 
-#filter n-grams
+## @knitr filter.map 
 filter.map = function(., lines) {
   ngram.data = ngram.format(lines)
   ngram.data[
     regexpr(
-      "^[a-z+'-]+$", 
+      "^[A-za-z+'-]+$", 
       ngram.data$ngram) > -1 & 
       ngram.data$year > 1700,]}
 
+## @knitr filtered.data
 filtered.data = 
   mapreduce(input = source,
             input.format = "text", #comment this on real data
             map = filter.map)
-  
+## @knitr sample-data
 from.dfs(rmr.sample(filtered.data, method="any", n = 50))
 
+## @knitr key.mult
 key.mult = function(k, n) 
   k + sample(0:(n-1), length(k), replace = TRUE)/n
 key.demult = floor
 
+## @knitr totals.map
 totals.map = 
   function(., ngram.data) {
     keyval(
       key.mult(ngram.data$year, 100),
       ngram.data$count)}
 
+## @knitr totals.reduce
 totals.reduce = 
   function(year.ish, count) 
     keyval(key.demult(year.ish), sum(count, na.rm = TRUE))
   
-#group counts by year and find totals
+## @knitr year.totals
 year.totals = 
   from.dfs(
     mapreduce(input = filtered.data,
@@ -92,74 +95,79 @@ year.totals =
               reduce = totals.reduce,
               combine = TRUE))
 
+## @knitr year.totals-finish
 year.totals = tapply(values(year.totals), keys(year.totals), sum)
 
 #year.totals = year.totals[-(1:77)]
 
-#group by yearish and compute p-values
-
-
+## @knitr outlier.map
 outlier.map = 
   function(., ngram.data) {
-    kk = ngram$year + cksum(ngram.data$ngram)%100/100
+    kk = ngram.data$year + cksum(ngram.data$ngram)%%100/100
     c.keyval(
       keyval(kk, ngram.data),
-      keyval(1 + kk, ngram.data))}
+      keyval(kk + 1, ngram.data))}
 
+## @knitr outlier.reduce
+library(robustbase)
 outlier.reduce =
-  function(ngram.data) {
-    years = range(ngram.data$years)
-    ngram.data = dcast(data=ngram.data, formula = ngram + count ~  year, fill = 0)
-    adjOutlyingness(
-      log(
-        t(
-          t(
-            ngram.data[,3:4])/year.totals[as.character(years)]))$nonOut
-    keyval(
-      key.mult(years[-1], 100), 
-      data.frame(ngram, log.freq[-length(log.freq)], log.freq[-1], stringsAsFactors = FALSE))}
+  function(., ngram.data) {
+    years = range(ngram.data$year)
+    if(years[1] == years[2])
+      NULL
+    else {
+      ngram.data = dcast(ngram.data, ngram ~ year, fill = 0)
+      cat(summary(ngram.data), file=stderr())
+      filter = 
+        !adjOutlyingness(
+          log(
+            t(
+              t(
+                ngram.data[,2:3] + 1)/as.vector(year.totals[as.character(years)] + 1))))$nonOut
+      as.character(ngram.data[filter,'ngram'])}}
 
-outlier.reduce = function(year.ish, ngram.data) {
-  keyval(NULL, unique(log.freqs[!adjOutlyingness(log.freqs[,2:3])$nonOut,1]))}
-
-outlier.ngrams = c()
-outlier.ngrams[
+## @knitr outlier.ngram
+outlier.ngram = c()
+outlier.ngram[
   unique(
-    unlist(
-      values(
-        from.dfs(
-          mapreduce(
-            input = filtered.data,
-            map = outlier.map,
-            reduce = outlier.reduce)))))] = TRUE
+    values(
+      from.dfs(
+        mapreduce(
+          input = filtered.data,
+          map = outlier.map,
+          reduce = outlier.reduce))))] = TRUE
 
+## @knitr ngram.filter
 ngram.filter = 
-  function(ngram) !is.na(outlier.ngrams[ngram])
-  
+  function(ngram) !is.na(outlier.ngram[as.character(ngram)])  
 
-graph.data = 
-  from.dfs(
-    mapreduce(
-      input = filter.norm.data,
-      map = function(ngram, data) if(ngram.filter(ngram)) keyval(ngram, data)))
-#visualization
+## @knitr plot.data
+plot.data = 
+  values(
+    from.dfs(
+      mapreduce(
+        input = filtered.data,
+        map = function(., ngram.data) 
+          ngram.data[ngram.filter(ngram.data$ngram),])))
 
-library(googleVis)
-graph.data.frame = cbind(keys(graph.data), values(graph.data))
-graph.data.frame = cbind(graph.data.frame[-nrow(graph.data.frame),],  graph.data.frame[-1,])
-graph.data.frame = graph.data.frame[graph.data.frame[1:10,1] == graph.data.frame[1:10,4],c(1,2,3,5,6)]
+## @knitr plot.data
+plot.data = cbind(plot.data[-nrow(plot.data),],  plot.data[-1,])
+plot.data = plot.data[plot.data[,1] == plot.data[,4],c(1,2,3,5,6)]
 
-names(graph.data.frame) = rmr2:::qw(id,time,count, time.1, count.1)
-graph.data.frame$average = 
+names(plot.data) = rmr2:::qw(id,time,count, time.1, count.1)
+plot.data$average = 
   as.vector(
     with(
-      graph.data.frame, 
+      plot.data, 
       sqrt((count/year.totals[as.character(time)]) * 
              (count.1/year.totals[as.character(time.1)]))))
-graph.data.frame$difference = 
+plot.data$ratio = 
   as.vector(
     with(
-      graph.data.frame, 
+      plot.data, 
       (count/year.totals[as.character(time)]) / 
         (count.1/year.totals[as.character(time.1)])))
-plot(gvisMotionChart(graph.data.frame[,c("id","time","average","difference")], options = list(height = 1000, width = 2000)))
+
+## @knitr plot
+library(googleVis)
+plot(gvisMotionChart(plot.data[,c("id","time","average","difference")], options = list(height = 1000, width = 2000)))
