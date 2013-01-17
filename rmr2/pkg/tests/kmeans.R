d0910acbc5bea47fbf38568a8149d091089e52ee
Antonio Piccolboni <antonio@piccolboni.info>
2013-01-16 23:08:54 -0800

    many formatting and other small refinements, and a simpler vectorized distance code

--- rmr2/pkg/tests/kmeans.R
+++ rmr2/pkg/tests/kmeans.R
@@ -16,42 +16,47 @@ library(rmr2)
 
 ## @knitr kmeans-signature
 kmeans.mr = 
-  function(P, num.clusters, num.iter, combine, in.mem.combine) {
+  function(
+    P, 
+    num.clusters, 
+    num.iter, 
+    combine, 
+    in.mem.combine) {
 ## @knitr kmeans-dist.fun
     dist.fun = 
       function(C, P) {
-        apply(C,
-              1, 
-              function(x) 
-                matrix(
-                  rowSums((t(t(P) - x))^2), 
-                  ncol = length(x)))}
-## @knitr kmeans.map.1
+        apply(
+          C,
+          1, 
+          function(x) 
+            colSums((t(P) - x)^2))}
+## @knitr kmeans.map
     kmeans.map = 
       function(., P) {
-        nearest = 
-          if(is.null(C)) {
+        nearest = {
+          if(is.null(C)) 
             sample(
               1:num.clusters, 
               nrow(P), 
-              replace = T)}
-        else {
-          D = dist.fun(C, P)
-          nearest = max.col(-D)}
+              replace = T)
+          else {
+            D = dist.fun(C, P)
+            nearest = max.col(-D)}}
         if(!(combine || in.mem.combine))
           keyval(nearest, P) 
         else 
           keyval(nearest, cbind(1, P))}
-## @knitr kmeans.reduce.1
+## @knitr kmeans.reduce
     kmeans.reduce = {
       if (!(combine || in.mem.combine) ) 
-        function(x, P) 
+        function(., P) 
           t(as.matrix(apply(P, 2, mean)))
-## @knitr kmeans.reduce.2
       else 
         function(k, P) 
-          keyval(k, t(as.matrix(apply(P, 2, sum))))}
-## @knitr kmeans-main    
+          keyval(
+            k, 
+            t(as.matrix(apply(P, 2, sum))))}
+## @knitr kmeans-main-1  
     C = NULL
     for(i in 1:num.iter ) {
       C = 
@@ -64,13 +69,15 @@ kmeans.mr =
       if(combine || in.mem.combine)
         C = C[, -1]/C[, 1]
       points(C, col = i + 1, pch = 19)
+## @knitr kmeans-main-2
       if(nrow(C) < num.clusters) {
         C = 
           rbind(
             C,
             matrix(
               rnorm(
-                (num.clusters - nrow(C)) * nrow(C)), 
+                (num.clusters - 
+                   nrow(C)) * nrow(C)), 
               ncol = nrow(C)) %*% C) }}
         C}
 ## @knitr end
@@ -94,14 +101,16 @@ for(be in c("local", "hadoop")) {
             ncol=2)), 
         20)) + 
     matrix(rnorm(200), ncol =2)
+  x11()
   plot(P)
+  points(P)
 ## @knitr end
   out[[be]] = 
 ## @knitr kmeans-run    
     kmeans.mr(
       to.dfs(P),
-      num.clusters  = 12, 
-      num.iter= 5,
+      num.clusters = 12, 
+      num.iter = 5,
       combine = FALSE,
       in.mem.combine = FALSE)
 ## @knitr end
