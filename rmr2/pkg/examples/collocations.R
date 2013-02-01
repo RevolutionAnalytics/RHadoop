236f31f89ca390cf32bb1f56d9a4ace25263ced5
Antonio Piccolboni <antonio@piccolboni.info>
2013-01-31 17:34:22 -0800

    formatting and a different experiment

--- rmr2/pkg/examples/collocations.R
+++ rmr2/pkg/examples/collocations.R
@@ -11,19 +11,32 @@ ngram.format =
 
 ngram.parse = 
   function(ngram.data) {
-    ngram.split = suppressWarnings(do.call(rbind, strsplit(paste(ngram.data$ngram, "     "), " "))[,1:5])
+    ngram.split = 
+      suppressWarnings(
+        do.call(
+          rbind, 
+          strsplit(
+            paste(ngram.data$ngram, "     "), 
+            " "))
+        [,1:5])
     filter = ngram.split[,ncol(ngram.split)] != "" 
-    cbind(ngram.data[,-1], ngram.split, stringsAsFactors = FALSE)[filter,]}
+    cbind(
+      ngram.data[,-1], 
+      ngram.split, 
+      stringsAsFactors = FALSE)
+    [filter,]}
 
 map.fun = 
   function(k, v) {
     data = ngram.parse(v)
-    keyval(as.matrix(data[, c("year", "1", names(data)[ncol(data)])]), data$count)}
+    keyval(
+      as.matrix(data[, c("year", "1", names(data)[ncol(data)])]), 
+      data$count)}
 
 reduce.fun = 
   function(k,vv) {
     vv = split(vv, as.data.frame(k), drop = TRUE)
-    keyval(names(vv), psum(vv))}
+    keyval(names(vv), vsum(vv))}
     #keyval(names(vv), sapply(vv, sum))} 
     #this alone changes the runtime from 49' to 1h 27' 
     #on a 5 node cluster with 10 reducer slots
@@ -37,5 +50,5 @@ system.time({
       map = map.fun, 
       reduce = reduce.fun,
       vectorized.reduce = TRUE,
-      in.memory.combine = TRUE,
+      in.memory.combine = FALSE,
       combine = FALSE)})
